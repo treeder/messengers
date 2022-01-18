588e7f45e73b0df461de8c140dc670d1b76b3a5b
Travis Reeder <treeder@gmail.com>
2022-01-18 15:39:18 +0000

    wip

--- discord/messenger.go
+++ discord/messenger.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"bytes"
+	"crypto/ed25519"
+	"encoding/json"
 
+	"github.com/bsdlp/discord-interactions-go/interactions"
 	"github.com/bwmarrin/discordgo"
 	"github.com/treeder/gotils/v2"
 	"github.com/treeder/messengers"
@@ -14,8 +18,10 @@ import (
 )
 
 type DiscordMessenger struct {
-	sess     *discordgo.Session
-	ClientID string
+	sess      *discordgo.Session
+	ClientID  string
+	PublicKey string
+	decodedPublicKey []byte 
 	// messageHandlers []messengers.MessageHandler
 	*messengers.BaseMessenger
 	baseCtx context.Context
@@ -177,5 +183,47 @@ func (mess *DiscordMessenger) AddHandler(ctx context.Context, h messengers.Messa
 }
 
 func (mess *DiscordMessenger) HandleEventHTTP(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "HTTP not supported with discord yet.", http.StatusNotImplemented)
+
+	// from: https://github.com/bsdlp/discord-interactions-go/blob/main/interactions/verify_example_test.go
+	verified := interactions.Verify(r, ed25519.PublicKey(mess.PublicKey))
+	if !verified {
+		http.Error(w, "signature mismatch", http.StatusUnauthorized)
+		return
+	}
+
+	defer r.Body.Close()
+	var data interactions.Data
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		// handle error
+	}
+
+	// respond to ping
+	if data.Type == interactions.Ping {
+		_, err := w.Write([]byte(`{"type":1}`))
+		if err != nil {
+			// handle error
+		}
+		return
+	}
+
+	// handle command
+	response := &interactions.InteractionResponse{
+		Type: interactions.ChannelMessage,
+		Data: &interactions.InteractionApplicationCommandCallbackData{
+			Content: "got your message kid",
+		},
+	}
+
+	var responsePayload bytes.Buffer
+	err = json.NewEncoder(&responsePayload).Encode(response)
+	if err != nil {
+		// handle error
+	}
+
+	_, err = http.Post(data.ResponseURL(), "application/json", &responsePayload)
+	if err != nil {
+		// handle err
+	}
+
 }
